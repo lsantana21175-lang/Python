cee9fd4e13846e906d61c51d16859f9f612ef37b
lsantana21175-lang <lsantana21175@iesjoanramis.org>
2025-11-12 12:57:01 +0000

    1

--- Prova1.py
+++ Prova1.py
@@ -1,4 +1,15 @@
-x = int(input("escriu el primer nombre"))
-y = int(input("Escriu el segon nombre"))
-z = x + y
-print("El resultat de la suma {} + {} és {}".format(x, y, z))
+a = float(input("Escriure el primer nombre "))
+b = float(input("Escriure el el segon nombre "))
+c = float(input("Escriu el tercer nombre "))
+d = a + b +d
+if d>20:
+    print("La suma és major a 20, és {}".format (d))
+else:
+    print("La suma és menor a 20, és {}".format(d))
+d = a * b * c
+if d>100:
+    print("El resultat de la multiplicacio es major a 100, es {}".format(d))
+else:
+    print("El resultat de la multiplicacio es major a 100, es {}".format(d))
+
+    
